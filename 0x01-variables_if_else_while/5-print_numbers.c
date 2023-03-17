ccf118a2ebc0008d7ab5127da6782a9304daf912
liyah <remote.liyah@gmail.com>
2023-03-17 16:05:38 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - single digit numbers of base 10 starting from 0.
+ *
+ * Return: always 0 (Succees)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
