67385ed9404fc085d93a4cc9c42eb894c1ba837c
liyah <remote.liyah@gmail.com>
2023-03-17 16:48:04 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,16 +9,12 @@
 int main(void)
 {
 	int i;
+	char j;
 
 	for (i = 0 ; i < 10 ; i++)
-	{
-		if (i == 9)
-			putchar(i + '0');
-		else
-		{	putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-		}
-	}
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
 	return (0);
 }
