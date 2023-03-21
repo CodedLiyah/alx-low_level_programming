34a8b8da4a70c92a7c92bbaef3680c999fc99498
liyah <remote.liyah@gmail.com>
2023-03-21 02:50:38 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+inclued "main.h"
+/**
+ * main - prints alphabet in lowercase.
+ *
+ * Return: always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+	return (0);
+}
