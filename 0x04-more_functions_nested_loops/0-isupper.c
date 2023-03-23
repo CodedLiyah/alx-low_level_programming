67ded4144e0187008c127e6f4d6a784dd8760602
liyah <remote.liyah@gmail.com>
2023-03-23 12:24:08 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks if the code is uppercase
+ * @c: is the int used for the function
+ * Return: 1 if uppercase character 0 if not
+ */
+int_isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
