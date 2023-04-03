9e79ab6c206338220c9bd91daad81f742c3f6d77
liyah <remote.liyah@gmail.com>
2023-04-03 13:11:41 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,33 +1,32 @@
 #include "main.h"
 /**
  * _strspn - length of a prefix substring
- * @s: the null-terminated string to be scanned
- * @accept: the null-terminated string containing the characters to match
+ * @s: the string to be scanned
+ * @accept: the string containing the characters to accept
  *
- * Return: returns the number of bytes in the initial segment
- * of s which consist of bytes from accept
+ * Return: returns the number of bytes
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int count = 0, flag;
+	unsigned int count = 0, r;
 	char *start = accept;
 
 	while (*s)
 	{
-		flag = 0;
+		r = 0;
 		while (*accept)
 		{
 			if (*accept)
 			{
 				count++;
-				flag = 1;
+				r = 1;
 				break;
 			}
 			accept++;
 		}
 		s++;
 		accept = start;
-		if (flag == 0)
+		if (r == 0)
 			break;
 	}
 	return (count);
