1d7952fcfe9a00561838d803316ea423bf10303e
liyah <remote.liyah@gmail.com>
2023-04-17 12:29:04 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _object_like_macro_h
-#define _object_like_macro_h
+#ifndef _object_like_macro_h_
+#define _object_like_macro_h_
 #define SIZE 1024
 
-#endif /*_object_like_macro_h_*/
+#endif /* _object_like_macro_h_ */
