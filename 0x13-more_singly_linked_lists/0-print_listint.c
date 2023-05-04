04751f5e32036330d415035c5f735e4645a06481
liyah <remote.liyah@gmail.com>
2023-05-04 12:04:31 -0700

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of a list
+ * @h: singly linked list
+ *
+ * Return: prints elements of a list
+ */
+size_t print_listint(const listint_t *h)
+{
+	int r;
+
+	r = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		r++;
+	}
+	
+	return(r);
+}
