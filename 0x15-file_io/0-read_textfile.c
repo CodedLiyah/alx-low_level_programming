f19641c017303e639f875eb1352c470f05397a25
liyah <remote.liyah@gmail.com>
2023-05-12 10:45:52 -0700

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,44 +2,34 @@
 #include <stddef.h>
 
 /**
- * read_textfile - A function that reads a text,print to POSIX standard output
- * @filename: name of file
- * @letters: number of letters to be printed
- *
- * Return: The actual number of letters read and printed
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file
+ * @letters:  number of letters to be printed
+ * CodedLiyah
+ * Return: number of letters read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t letters;
-	int file;
+	int file, n_read, wrote;
 	char *buffer;
 
-	if (!filename)
+	buffer = malloc(sizeof(*buffer) * (letters + 1));
+	if (filename == NULL || buffer == NULL)
+	{
+		free(buffer);
 		return (0);
-	buffer = malloc(sizeof(char) * letters + 1);
-	if (text == NULL)
-		return (0);
-	file = open(filename, 0_RDONLY);
+	}
+	file = open(filename, O_RDONLY);
 	if (file == -1)
-	{
-		free(text);
 		return (0);
-	}
-	letters = read(file, text, sizeof(char) * letters);
-	if (letters == -1)
-	{
-		free(text);
-		close(file);
+	n_read = read(file, buffer, letters);
+	if (n_read == -1)
 		return (0);
-	}
-	letters = write(STDOUT_FILENO, text, letters);
-	if (letters == -1)
-	{
-		free(text);
-		close(file);
+	buffer[n_read] = '\0';
+	wrote = write(STDOUT_FILENO, buffer, n_read);
+	if (wrote != n_read)
 		return (0);
-	}
-	free(text);
+	free(buffer);
 	close(file);
-	return (letters);
+	return (n_read);
 }
